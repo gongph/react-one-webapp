d951b2a36b28833130a5843b4c825251403ee3e3
gongph <gongph@foxmail.com>
2017-08-10 14:29:59 +0800

    add question detail page router

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ import { render } from 'react-dom'
 import { HashRouter as Router, Route } from 'react-router-dom'
 import IndexPage from './components/IndexPage.js'
 import Article from './components/Article.js'
+import Question from './components/Question.js'
 
 render((
   <Router>
     <div>
       <Route exact path="/" component={IndexPage}/>
       <Route path="/article/:id" component={Article}/>
+      <Route path="/question/:id" component={Question}/>
     </div>
   </Router>
 ), document.getElementById('app'))
