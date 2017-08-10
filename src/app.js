c846650255a25272079a433302bde479e5ad32d1
gongph <gongph@foxmail.com>
2017-08-10 16:02:23 +0800

    add about page router

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { HashRouter as Router, Route } from 'react-router-dom'
 import IndexPage from './components/IndexPage.js'
 import Article from './components/Article.js'
 import Question from './components/Question.js'
+import About from './components/About.js'
 
 render((
   <Router>
@@ -11,6 +12,7 @@ render((
       <Route exact path="/" component={IndexPage}/>
       <Route path="/article/:id" component={Article}/>
       <Route path="/question/:id" component={Question}/>
+      <Route path="/acercade" component={About}/>
     </div>
   </Router>
 ), document.getElementById('app'))
